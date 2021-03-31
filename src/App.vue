81a119c324a908c2198338bdaf0bb48e876dd03c
Ademola409 <ademolal353@gmail.com>
2021-03-31 05:53:45 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <base-container title="Vuex" v-if="isAuth">
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+  
+  <base-container title='Auth'>
+    <user-auth></user-auth> 
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import theCounter from './components/theCounter.vue';
+import changeCounter from './components/changeCounter.vue';
+import favoriteValue from './components/favoriteValue.vue';
+import userAuth from './components/UserAuth.vue';
+export default {
+  components: {
+    BaseContainer,
+    theCounter,
+    changeCounter,
+    favoriteValue,
+    userAuth
+  },
+  computed:{
+    isAuth(){
+      return this.$store.getters.userIsAuthenticated
+    }
+  },
+  methods:{
+    addOne(){
+      this.$store.dispatch('numbers/increase', {value:10})
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+
+</style>
---
+++ src/components/favoriteValue.vue
@@ -0,0 +1,15 @@
+<template>
+    {{counter}}
+    <p>we do more....</p>
+</template>
+
+
+<script>
+export default {
+    computed:{
+        counter(){
+            return this.$store.getters['numbers/normalizedCounter']  
+        }
+    },
+}
+</script>
